042a7de72898fa0305073034b8fa818ff0bf94e4
AnnaZakiah <annazakiaamini@gmail.com>
2022-11-11 09:27:21 +0700

    struktur kendali

---
+++ StrukturKendali/latihan1.1.php
@@ -0,0 +1,37 @@
+<?php
+
+// PENGULANGAN
+// FOR
+// for ( $i = 0; $i < 5; $i++ ) {
+//     echo "Hello World <br>";
+// }
+
+
+// WHILE
+// $i = 0;
+// while ($i < 5 ) {
+//     echo "Hello World <br>";
+// $i++;
+// }
+
+
+// DO...WHILE
+// $i = 0;
+// do {
+//     echo "Hello World <br>";
+// $i++;
+// } while ( $i < 5 );
+
+
+// FOREACH : PENGULANGAN KHUSUS ARRAY
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ StrukturKendali/latihan2.php
@@ -0,0 +1,30 @@
+<?php
+
+// PENGKONDISIAN / PERCABANGAN
+//  IF ELSE
+// $x = 20;
+// if ( $x < 20 ) {
+//     echo "benar";
+// } else {
+//     echo "salah";
+// }
+
+
+//  IF ELSE IF ELSE
+$x = 20;
+if ( $x < 20 ) {
+    echo "benar";
+} else if ( $x == 20 ) {
+    echo 'bingo';
+} else {
+    echo "salah";
+}
+
+
+//  TERNARY
+//  SWITCH
+
+
+
+
+?>
